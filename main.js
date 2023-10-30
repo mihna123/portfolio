ee94162c989e2d1fcfce77f1a20be3b0c0c96bc6
mihailo <mihailonvojinovic@gmail.com>
2023-10-30 21:48:28 +0100

    Add backdrop

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const pause_button = document.getElementById("pause-btn");
-const pause_menu = document.getElementById("pause-menu");
+const pause_backdrop = document.getElementById("pause-backdrop");
+const pause_icon = pause_button.innerHTML;
+const play_icon = "<i class=\"fa-solid fa-play\" style=\"color: #ffffff;\"></i>";
 
-pause_menu.style["display"] = "none";
+pause_backdrop.style["display"] = "none";
 
 
 pause_button.addEventListener("click", function() {
-    if(pause_menu.style["display"] === "none") {
-        pause_menu.style["display"] = "initial";
+    if(pause_backdrop.style["display"] === "none") {
+        pause_backdrop.style["display"] = "initial";
+        pause_button.innerHTML = play_icon;
         return;
     }
-    pause_menu.style["display"] = "none";
+    pause_backdrop.style["display"] = "none";
+    pause_button.innerHTML = pause_icon;
 });
